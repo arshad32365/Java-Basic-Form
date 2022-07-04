bfa0d0cbda91bb13884b42c1390c72d02c3dc13d
Arshad Ali <72139162+arshad32365@users.noreply.github.com>
2022-07-04 21:11:44 +0500

    Add files via upload

---
+++ src/MyWindow.java
@@ -0,0 +1,64 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+class MyWindow extends JFrame {
+    private JLabel heading;
+    Font font  = new Font ("", Font.BOLD, 30);
+    private JPanel mainPanel;
+    private JLabel nameLabel, passwordLabel;
+    private JTextField nameTextField;
+    private JPasswordField passwordField;
+    private JButton button1, button2;
+    public MyWindow(){
+        super.setTitle("My First Form");
+        super.setSize(600, 600);
+        super.setLocation(100, 100);
+        super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.createGUI();
+
+        super.setVisible(true);
+        System.out.println("My constructor");
+    }
+    public void createGUI(){
+        this.setLayout(new BorderLayout());
+        heading = new JLabel("My First Form");
+        heading.setFont(font);
+        heading.setHorizontalAlignment(JLabel.CENTER);
+
+        this.add(heading, BorderLayout.NORTH);
+
+        // working with JPanel
+
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new GridLayout(3, 2));
+
+        nameLabel = new JLabel("Enter Name:");
+        nameLabel.setFont(font);
+
+        passwordLabel = new JLabel("Enter Password:");
+        passwordLabel.setFont(font);
+        nameTextField = new JTextField();
+        nameTextField.setFont(font);
+
+        passwordField = new JPasswordField();
+        passwordField.setFont(font);
+
+        button1 = new JButton("Submit");
+        button1.setFont(font);
+        button2 = new JButton("Reset");
+        button2.setFont(font);
+
+        mainPanel.add(nameLabel);
+        mainPanel.add(nameTextField);
+        mainPanel.add(passwordLabel);
+        mainPanel.add(passwordField);
+        mainPanel.add(button1);
+        mainPanel.add(button2);
+
+        this.add(mainPanel, BorderLayout.CENTER);
+
+
+
+    }
+}
